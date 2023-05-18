beaac2a256f595cd2da5f78e2f4297e5abdf033f
clebermo <clebermoliveira@gmail.com>
2023-05-18 10:26:01 -0300

    Exercício curso Java Matriz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,5 +31,40 @@ public class Main {
         System.out.println("d06 = " + fmt3.format(d06));
         System.out.println("d06 = " + fmt5.format(d06));
         System.out.println("d06 = " + d06);
+        Scanner leitor = new Scanner(System.in);
+
+        int m = leitor.nextInt();
+        int n = leitor.nextInt();
+
+        int[][] matriz = new int[m][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = leitor.nextInt();
+            }
+        }
+
+        int x = leitor.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + matriz[i][j - 1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + matriz[i - 1][j]);
+                    }
+                    if (j < matriz[i].length - 1) {
+                        System.out.println("Right: " + matriz[i][j + 1]);
+                    }
+                    if (i < matriz.length - 1) {
+                        System.out.println("Down: " + matriz[i + 1][j]);
+                    }
+                }
+            }
+        }
+        leitor.close();
     }
 }
